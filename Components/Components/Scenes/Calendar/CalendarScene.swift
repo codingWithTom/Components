8a5248feacec586aae1fd815cbe81ef343ab6dc1
Coding with Tomi <codingwithtom138@gmail.com>
2023-02-09 17:18:48 -0800

    First Video

--- Components/Components/Scenes/Calendar/CalendarScene.swift
+++ Components/Components/Scenes/Calendar/CalendarScene.swift
@@ -8,18 +8,55 @@
 import SwiftUI
 
 struct CalendarScene: View {
-    var body: some View {
+  @State private var selectedIdentifier: Calendar.Identifier = .gregorian
+  @State private var selectedDate: Date?
+  
+  private var textDate: String {
+    guard let selectedDate else { return "Date not selected" }
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    return formatter.string(from: selectedDate)
+  }
+  
+  var body: some View {
+    ScrollView {
       VStack {
         Text("Calendars Go Here!")
-          .font(.largeTitle)
+          .font(.title)
+        Text(textDate)
+          .font(.title3)
         Spacer()
+        CalendarView(canSelect: true,
+                     selectedDate: $selectedDate)
+          .scaledToFit()
+        Spacer()
+        Picker("", selection: $selectedIdentifier) {
+          Text("Gregorian")
+            .tag(Calendar.Identifier.gregorian)
+          Text("Hebrew")
+            .tag(Calendar.Identifier.hebrew)
+          Text("Chinese")
+            .tag(Calendar.Identifier.chinese)
+          Text("Buddhist")
+            .tag(Calendar.Identifier.buddhist)
+          Text("Coptic")
+            .tag(Calendar.Identifier.coptic)
+          Text("Indian")
+            .tag(Calendar.Identifier.indian)
+          Text("Islamic")
+            .tag(Calendar.Identifier.islamic)
+        }
+        CalendarView(calendarIdentifier: selectedIdentifier,
+                     selectedDate: $selectedDate)
+          .scaledToFit()
       }
-      .padding()
     }
+    .padding(.horizontal)
+  }
 }
 
 struct CalendarScene_Previews: PreviewProvider {
-    static var previews: some View {
-        CalendarScene()
-    }
+  static var previews: some View {
+    CalendarScene()
+  }
 }
